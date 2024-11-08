b6ea262922aafd30cd442e01af1011a433932e96
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2024-11-07 19:39:19 -0600

    Commit Changes

---
+++ car.js
@@ -0,0 +1,6 @@
+let make = "Toyota"
+
+exports.details = function(){
+
+    return ` This car make is ${make} `
+}
--- server.js
+++ server.js
@@ -1,18 +1,41 @@
 var http = require('http')
+var car = require('./car.js')
+var url = require('url')
+// http =  Hyper Text Transfer Protocol 
+// HTML  = Hyper Text Markup Language
+// <a> <h1> <p>
+
+// ftp = file transfer protocol (dropbox, google drive)
+
+// smtp = email (gmail, outlook, yahoo)
 
 http.createServer( serverPurpose ).listen(3000)
 
 function serverPurpose(req, res){
-    res.writeHead(200, {'Content-Type': 'text/html'})
+    res.writeHead(200, {'Content-Type': 'application/html'})
+    // Create a Project with 2 routes 
+    // perimeter: perform the operation of calculating the perimeter of an square 
 
-    // create an algorithm to calculate the perimeter of a Square
+    // add: perform the operation of calculation the addtion of 2 numbers
 
-    let sidea = 10
-    let sideb = 10
-    let perimeter = (sidea*2)+(sideb*2)
+    res.write("<h1>Hello!! </h1>")
 
-    res.write(`<h1>Hello</h1>`)
-    res.write(`<p>This is my first node Js Project</p>`)
-    res.write(`The perimeter is ${perimeter}`)
+    let curlyParameters = url.parse(req.url, true).query
+    let urlData  = url.parse(req.url)
+
+    if (urlData.pathname === '/perimeter'){
+        let sidea = parseInt(curlyParameters.sidea) 
+        let sideb = parseInt(curlyParameters.sideb) 
+        let perimeter =(sidea*2) + (sideb*2)
+        res.write(`The perimeter is ${perimeter}`)
+    }
+
+    if (urlData.pathname === '/add'){
+        let num1 = parseInt(curlyParameters.num1) 
+        let num2 = parseInt(curlyParameters.num2)
+        let addition = num1 + num2
+        res.write(`The addition is ${addition}`)
+    }
+
     res.end(`<h2>Good bye</h2>`)
 }
